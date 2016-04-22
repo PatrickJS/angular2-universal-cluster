ca2186073494742230ab9fcce14011c9632fb626
gdi2290 <github@gdi2290.com>
2016-04-22 13:20:03 -0700

    feat: server-cluster.js

---
+++ server-cluster.js
@@ -0,0 +1,23 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+if (cluster.isMaster) {
+
+  console.log('CPU count', numCPUs);
+
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('online', function(worker) {
+    console.log('Worker ' + worker.process.pid + ' is online');
+  });
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
+} else {
+  // Server
+  require('./dist/server/bundle.js');
+}
